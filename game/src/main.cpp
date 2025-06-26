72e076925a8d58aeadf163abec1f7bafd36a5616
Smiley98 <smileysofficialcinema@gmail.com>
2025-06-26 10:44:40 -0400

    Added blank project

--- game/src/main.cpp
+++ game/src/main.cpp
@@ -1,43 +1,20 @@
 #include "raylib.h"
 #include "raymath.h"
 
-void GameInit()
+int main()
 {
     SetConfigFlags(FLAG_VSYNC_HINT | FLAG_WINDOW_RESIZABLE);
     InitWindow(800, 800, "Game");
-    SetTargetFPS(144);
-
-    TraceLog(LOG_INFO, "Connor TODO - Make the map so we have a place to test our game!\n");
-}
-
-void GameCleanup()
-{
-    CloseWindow();
-}
-
-void GameUpdate(float dt)
-{
-
-}
-
-void GameDraw()
-{
-    BeginDrawing();
-    ClearBackground(DARKGRAY);
-    DrawText("Hello World!", 10, 10, 20, RED);
-    EndDrawing();
-}
-
-int main()
-{
-    GameInit();
+    SetTargetFPS(GetMonitorRefreshRate(GetCurrentMonitor()));
 
     while (!WindowShouldClose())
     {
-        GameUpdate(GetFrameTime());
-        GameDraw();
+        BeginDrawing();
+        ClearBackground(DARKGRAY);
+        DrawText("Hello World!", 10, 10, 20, RED);
+        EndDrawing();
     }
 
-    GameCleanup();
+    CloseWindow();
     return 0;
 }
